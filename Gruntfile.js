7e65bbacddcc77b2af00fcb0e7b29590d7b6496a
build server <ian.drosos@gmail.com>
2016-10-16 14:46:53 +0000

    separated test from static analysis

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,8 +92,8 @@ module.exports = function(grunt) {
 
   grunt.registerTask('all', ['test', 'build', 'uglify']);
   grunt.registerTask('default', ['all']);
-  grunt.registerTask('test', 'Runs all tests and linting', ['node_tap', 'jshint']);
-  //grunt.registerTask('test', 'Runs all tests and linting', ['jshint']);
+  grunt.registerTask('test', 'Runs all tests and linting', ['node_tap']);
+  grunt.registerTask('static', 'Runs all tests and linting', ['jshint']);
   grunt.loadNpmTasks('grunt-node-tap');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
