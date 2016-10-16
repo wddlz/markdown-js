6bb1a50384416cbd483fbca6e72557cd3d2d5063
Debalin Das <debalin90@gmail.com>
2016-10-16 17:10:03 -0400

    Test commit.

--- src/markdown_helpers.js
+++ src/markdown_helpers.js
@@ -11,7 +11,7 @@ define([], function (w) {
             ", " +
             uneval(this.trailing) +
             ", " +
-            uneval(this.lineNumber) +
+            uneval(this.lineNumber) + 
             " )";
   }
 
