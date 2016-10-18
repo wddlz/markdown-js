d4570b07e952a3dbaad157f4aa8026d3aaa88613
Pratik Mukherjee <pmukher@ncsu.edu>
2016-10-18 17:45:41 -0400

    deleting a comment

--- src/markdown.js
+++ src/markdown.js
@@ -1,6 +1,5 @@
 //Adding comment 
 //Additon a comment 
-//Adding a comment
 if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
 // Include all our dependencies and return the resulting library.
