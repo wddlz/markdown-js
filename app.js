987a40e7fd210406ddfb73be0081ec70b57bbca5
kkapoor <kkapoor@ncsu.edu>
2016-11-06 17:47:23 -0500

    added app.js simple server

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var fs      = require('fs')
+var app = express()
+
+var args = process.argv.slice(2);
+var PORT = args[0];
+PORT = 8080;
+
+// Root page
+app.get('/', function(req, res) {
+    res.send("Hello World");
+});
+
+var server = app.listen(PORT, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+});
