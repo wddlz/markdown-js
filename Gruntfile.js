87976e673cd3e6e79952686b966242a328ea3f0b
kkapoor <kkapoor@ncsu.edu>
2016-11-06 19:03:16 -0500

    removed trailing whitespace in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
     },
 
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'], 
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
         "browser": false,
         "maxerr": 100,
