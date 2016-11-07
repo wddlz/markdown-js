83aa1d6300a5331f6e7d308cc1afcd6409411184
Ian Drosos <wddlz@users.noreply.github.com>
2016-11-07 14:14:26 -0500

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var fs      = require('fs')
 var app = express()
+// REDIS
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
 
 var args = process.argv.slice(2);
 var PORT = args[0];
@@ -8,7 +11,15 @@ PORT = 9090;
 
 // Root page
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    var message = "Hellow World";
+    client.exists('featureFlag', function(err, reply) {
+        if (reply === 1) {
+            message = "NEW FEATURE";
+        } else {
+            console.log('feature flag off');
+        }
+    }
+    res.send(message);
 });
 
 var server = app.listen(PORT, function () {
