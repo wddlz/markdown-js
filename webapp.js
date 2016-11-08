a564a2b8345d2c7a6e71c40acb0bbd1fc35efb15
Debalin Das <debalin90@gmail.com>
2016-11-08 11:23:42 -0500

    Getting redis address from environment variables.

--- webapp.js
+++ webapp.js
@@ -1,66 +1,66 @@
-var express = require('express')
-var fs      = require('fs')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var app = express();
 // REDIS
-var redis = require('redis')
-var client = redis.createClient(6379, '127.0.0.1', {})
+var redis = require('redis');
+var client = redis.createClient(6379, process.env.DB_PORT_6379_TCP_ADDR, {});
 
 var args = process.argv.slice(2);
 var PORT = args[0];
 PORT = 9090;
 
 // Root page
-app.get('/', function(req, res) {
-    var message = "Hello World";
-    client.exists('featureFlag', function(err, reply) {
-        if (reply === 1) {
-            console.log('feature flag on');
-            message = "NEW FEATURE";
-        } else {
-            console.log('feature flag off');
-        }
-        res.send(message);
-    });
+app.get('/', function (req, res) {
+  var message = "Hello World";
+  client.exists('featureFlag', function (err, reply) {
+    if (reply === 1) {
+      console.log('feature flag on');
+      message = "NEW FEATURE";
+    } else {
+      console.log('feature flag off');
+    }
+    res.send(message);
+  });
 });
 
 app.get('/get', function (req, res) {
-        client.get("selfdestruct", function (err, value) {
-            console.log(value);
-            res.send(value);
-        });
+  client.get("selfdestruct", function (err, value) {
+    console.log(value);
+    res.send(value);
+  });
 });
 
 app.get('/set', function (req, res) {
-        var message = "this message will self destruct in 10 seconds";
-        // if (err) throw err
-        // res.writeHead(200, {'content-type':'text/html'});
-        client.set("selfdestruct", message);
-        client.expire("selfdestruct", 10);
-        client.get("selfdestruct", function (err, value) {
-            res.send("KEY selfdestruct SET TO " + value);
-        })
+  var message = "this message will self destruct in 10 seconds";
+  // if (err) throw err
+  // res.writeHead(200, {'content-type':'text/html'});
+  client.set("selfdestruct", message);
+  client.expire("selfdestruct", 10);
+  client.get("selfdestruct", function (err, value) {
+    res.send("KEY selfdestruct SET TO " + value);
+  })
 });
 
 // /set/:message to post params
 app.get('/set/:message', function (req, res) {
-        client.get("setMessageFlag", function(err, reply) {
-            if (reply == 1) {
-                var message = req.params.message;
-                client.set("selfdestruct", message);
-                client.expire("selfdestruct", 10);
-                client.get("selfdestruct", function (err, value) {
-                    res.send("KEY selfdestruct SET TO " + value);
-                });
-            } else {
-                res.send("FEATURE FLAG setMessageFlag OFF, FEATURE DISABLED");
-            }
-        });
+  client.get("setMessageFlag", function (err, reply) {
+    if (reply == 1) {
+      var message = req.params.message;
+      client.set("selfdestruct", message);
+      client.expire("selfdestruct", 10);
+      client.get("selfdestruct", function (err, value) {
+        res.send("KEY selfdestruct SET TO " + value);
+      });
+    } else {
+      res.send("FEATURE FLAG setMessageFlag OFF, FEATURE DISABLED");
+    }
+  });
 });
 
 var server = app.listen(PORT, function () {
 
-    var host = server.address().address
-    var port = server.address().port
+  var host = server.address().address
+  var port = server.address().port
 
-    console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port)
 });
